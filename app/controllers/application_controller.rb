79ab6f3980489c3dbdfbc84402044bbf445bb0a6
Christian Girling <girling.christian@gmail.com>
2022-03-11 06:15:22 -0800

    Added commented filter if needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Uncomment if restricted access is wanted
+  # before_filter :authorize
+
   private
 
   def cart
