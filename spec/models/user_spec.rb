b51c44b86c25dfb5564fe4a825d290c2e5e0d68d
Christian Girling <girling.christian@gmail.com>
2022-03-15 15:52:14 -0700

    Added testing for user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context 'User passwords match' do
+    user = User.new(email: 'cassilda@carcosa.com', password: 'hyades', password_confirmation: 'hyades')
+    user.valid?
+    it 'Userpassword match' do
+      expect(user.errors[:password_confirmation]).not_to be_present
+    end
+  end
 
+  context "User password don't match" do
+    user = User.new(email: 'cassilda@carcosa.com', password: 'hyades', password_confirmation: 'tatteredking')
+    user.valid?
+    it "User password don't match" do
+      expect(user.errors[:password_confirmation]).to be_present
+    end
+  end
+
+  it 'User email must be unique' do
+    user = User.new(email: 'cassilda@carcosa.com', password: 'hyades', password_confirmation: 'hyades')
+    user.save
+
+    u = User.new(email: 'cassilda@carcosa.com', password: 'hyades', password_confirmation: 'hyades')
+    u.save
+
+    expect(u.errors[:email].first).to eq('has already been taken')
+  end
+
+  it 'password length less than 5 characters is invalid' do
+    user = User.new(email: 'cassilda@carcosa.com', password: 'king')
+
+    result = user.save
+
+    expect(result).to be(false)
+  end
+
+  it 'password length must be at-least 5 characters' do
+    user = User.new(email: 'cassilda@carcosa.com', password: 'hyades')
+
+    result = user.save
+
+    expect(result).to be(true)
+  end
 end
