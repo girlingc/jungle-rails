da64bece870aa6aa68eca86332c16ab3e52c8cc3
Christian Girling <girling.christian@gmail.com>
2022-03-16 13:59:51 -0700

    Created test for product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    visit root_path
+
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
+
+    find('a.btn-default').click
+
+    expect(page).to have_content('Description')
+
+    save_screenshot
+  end
+
+end
